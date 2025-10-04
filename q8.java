b55b5e79fd77101264d4431e30112240d3abfefe
Nehal Panchal <nehalpanchal414@gmail.com>
2025-10-04 21:50:29 +0530

    code

---
+++ q8.java
@@ -0,0 +1,18 @@
+// Write a Java program that takes a year from the user and print whether that year is a leap year or not.
+
+import java.util.*;
+
+public class q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
